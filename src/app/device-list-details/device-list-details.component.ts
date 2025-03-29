29e9dcb815360ac966e4ecdd4d7e373ead1d773f
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2025-03-29 01:21:10 -0400

    Add iframe to view device page when clicking it

--- src/app/device-list-details/device-list-details.component.ts
+++ src/app/device-list-details/device-list-details.component.ts
@@ -3,25 +3,38 @@ import { MatListModule } from '@angular/material/list';
 import { DeviceListItemComponent } from '../device-list-item/device-list-item.component';
 import { DeviceService, DeviceWithState } from '../device.service';
 import { Device } from '../../lib/database/device';
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-device-list-details',
-  imports: [MatListModule, DeviceListItemComponent],
+  imports: [MatListModule, DeviceListItemComponent, CommonModule],
   templateUrl: './device-list-details.component.html',
   styleUrl: './device-list-details.component.scss',
 })
 export class DeviceListDetailsComponent implements OnInit {
-  constructor(private deviceService: DeviceService) {}
+  devicesWithState: DeviceWithState[] = [];
+  selectedDeviceWithState: DeviceWithState | null = null;
+  selectedDeviceAddress: SafeResourceUrl | null = null;
+
+  constructor(
+    private deviceService: DeviceService,
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit() {
     this.getDevices();
   }
 
-  devicesWithState: DeviceWithState[] = [];
-
   getDevices() {
     this.deviceService.devicesWithState$.subscribe(devicesWithState => {
       this.devicesWithState = devicesWithState;
     });
+  }
+
+  setSelectedDevice(deviceWithState: DeviceWithState) {
+    this.selectedDeviceWithState = deviceWithState;
+    this.selectedDeviceAddress = this.sanitizer.bypassSecurityTrustResourceUrl('http://' + deviceWithState.device.address);
+    console.log(this.selectedDeviceAddress);
   }
 }
