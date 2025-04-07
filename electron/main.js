188c59ad492216edc2e162bbbcf2f83578cdfbdb
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2025-04-06 22:06:51 -0400

    Change min size of the window

--- electron/main.js
+++ electron/main.js
@@ -19,6 +19,8 @@ function createWindow() {
   win = new BrowserWindow({
     width: 1200,
     height: 800,
+    minWidth: 700,
+    minHeight: 500,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
