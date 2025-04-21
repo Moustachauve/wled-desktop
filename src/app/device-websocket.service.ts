979cd1e80f86668dfb4bab14e93f512d69ad1b64
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2025-04-20 21:56:08 -0400

    Refactor websocket service to use RxJS websocket

--- src/app/device-websocket.service.ts
+++ src/app/device-websocket.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, OnDestroy } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { plainToInstance } from 'class-transformer';
+import { Observable, retry, Subject, timer } from 'rxjs';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import { db } from '../lib/database/db';
 import { Device } from '../lib/database/device';
-import { DeviceStateInfo, ParseDeviceJsonState } from '../lib/device-api-types';
+import { DeviceStateInfo } from '../lib/device-api-types';
 import { DeviceService } from './device.service';
 
 export interface DeviceWebsocketState {
@@ -29,7 +31,7 @@ export class DeviceWithState {
   providedIn: 'root',
 })
 export class DeviceWebsocketService implements OnDestroy {
-  private deviceWebsockets: Record<string, WebSocket> = {};
+  private deviceWebsockets: Record<string, WebSocketSubject<unknown>> = {};
   private devicesWithState: Record<string, DeviceWithState> = {};
   private devicesWithStateSubject = new Subject<DeviceWithState[]>();
 
@@ -56,7 +58,7 @@ export class DeviceWebsocketService implements OnDestroy {
       .filter(macAddress => !currentMacAddresses.includes(macAddress))
       .forEach(macAddress => {
         if (this.deviceWebsockets[macAddress]) {
-          this.deviceWebsockets[macAddress].close();
+          this.deviceWebsockets[macAddress].complete();
           delete this.deviceWebsockets[macAddress];
           delete this.devicesWithState[macAddress];
         }
@@ -73,7 +75,6 @@ export class DeviceWebsocketService implements OnDestroy {
 
   openWebsocket(device: Device) {
     const websocketUrl = `ws://${device.address}/ws`;
-    let ws: WebSocket;
     let reconnectAttempts = 0;
     const maxReconnectAttempts = 5; // Adjust as needed
     const reconnectDelay = 15000; // Adjust delay in milliseconds
@@ -82,90 +83,68 @@ export class DeviceWebsocketService implements OnDestroy {
       `Initiating WebSocket connection for device ${device.macAddress}...`
     );
 
-    const connect = () => {
-      ws = new WebSocket(websocketUrl);
+    this.deviceWebsockets[device.macAddress] = webSocket(websocketUrl);
 
-      ws.onopen = () => {
-        console.log(
-          `WebSocket connected to device ${device.macAddress}:${device.address}`
-        );
-        this.deviceWebsockets[device.macAddress] = ws;
-        this.devicesWithState[device.macAddress].isWebsocketConnected = true;
-        reconnectAttempts = 0; // Reset attempts on successful connection
-        this.publishDevicesWithState();
-      };
+    this.deviceWebsockets[device.macAddress]
+      .pipe(
+        // Use exponential backoff to retry connecting to the device
+        retry({
+          count: maxReconnectAttempts,
+          delay: (_error, retryIndex) => {
+            const interval = reconnectDelay;
+            // Double the interval each time it fails to connect
+            const delay = Math.pow(2, retryIndex - 1) * interval;
+            console.log('retrying connection...', _error, retryIndex);
+            return timer(delay);
+          },
+        })
+      )
+      .subscribe({
+        next: message => {
+          console.log('onmessage', device.macAddress, message);
+          const deviceState = plainToInstance(DeviceStateInfo, message);
+          this.devicesWithState[device.macAddress].stateInfo = deviceState;
 
-      ws.onmessage = event => {
-        const deviceState = ParseDeviceJsonState(event.data);
-        this.devicesWithState[device.macAddress].stateInfo = deviceState;
-
-        // Update stored device state
-        // TODO: Should be extracted to its own function probably or done better.
-        this.devicesWithState[device.macAddress].device.originalName =
-          deviceState?.info.name;
-        db.devices.update(
-          device.macAddress,
-          this.devicesWithState[device.macAddress].device
-        );
-
-        console.log(
-          'onmessage',
-          device.macAddress,
-          this.devicesWithState[device.macAddress].stateInfo
-        );
-        this.publishDevicesWithState();
-      };
-
-      ws.onclose = () => {
-        console.log(`WebSocket closed for device ${device.macAddress}`);
-        delete this.deviceWebsockets[device.macAddress];
-        if (this.devicesWithState[device.macAddress]) {
-          this.devicesWithState[device.macAddress].isWebsocketConnected = false;
-        }
-        this.publishDevicesWithState();
-        // Only try to reconnect if the device is still in the list of devicesWithState
-        if (this.devicesWithState[device.macAddress]) {
-          if (reconnectAttempts < maxReconnectAttempts) {
-            reconnectAttempts++;
-            console.log(
-              `Attempting reconnection to ${device.macAddress} in ${reconnectDelay}ms...` +
-                ` (Attempt ${reconnectAttempts}/${maxReconnectAttempts})`
-            );
-            setTimeout(connect, reconnectDelay);
-          } else {
-            console.error(
-              `Max reconnection attempts reached for ${device.macAddress}.`
-            );
-          }
-        } else {
-          console.log(
-            `Won't reconnect to ${device.macAddress}, it's no longer in the list.`
+          // Update stored device state
+          this.devicesWithState[device.macAddress].isWebsocketConnected = true;
+          // TODO: Should be extracted to its own function probably or done better.
+          this.devicesWithState[device.macAddress].device.originalName =
+            deviceState?.info.name;
+          db.devices.update(
+            device.macAddress,
+            this.devicesWithState[device.macAddress].device
           );
-        }
-      };
-
-      ws.onerror = error => {
-        this.devicesWithState[device.macAddress].isWebsocketConnected = false;
-        this.publishDevicesWithState();
-        console.error(
-          `WebSocket error for device ${device.macAddress}:`,
-          error
-        );
-      };
-    };
-
-    connect(); // Initiate the initial connection
+          this.publishDevicesWithState();
+        },
+        error: error => {
+          this.devicesWithState[device.macAddress].isWebsocketConnected = false;
+          this.publishDevicesWithState();
+          console.error(
+            `WebSocket error for device ${device.macAddress}:`,
+            error
+          );
+        },
+        complete: () => {
+          console.log(`WebSocket closed for device ${device.macAddress}`);
+          //delete this.deviceWebsockets[device.macAddress];
+          if (this.devicesWithState[device.macAddress]) {
+            this.devicesWithState[device.macAddress].isWebsocketConnected =
+              false;
+          }
+          this.publishDevicesWithState();
+        },
+      });
   }
 
   sendMessage(macAddress: string, message: object) {
     if (!macAddress || !this.deviceWebsockets[macAddress]) return;
-
-    this.deviceWebsockets[macAddress].send(JSON.stringify(message));
+    console.log(`Sending message to ${macAddress}:`, message);
+    this.deviceWebsockets[macAddress].next(message);
   }
 
   closeAllWebsockets() {
     Object.values(this.deviceWebsockets).forEach(ws => {
-      ws.close();
+      ws.complete();
     });
     this.deviceWebsockets = {};
     this.devicesWithState = {};
