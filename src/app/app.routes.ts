9c5c27959b56eed9973b693ab9dedc32b871834b
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2025-05-02 00:46:28 -0400

    Add ability to refresh the page and open the dev tools

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'index.html', component: NavigationComponent, pathMatch: 'full' },
+  { path: '', redirectTo: 'index.html', pathMatch: 'full' },
+  { path: '**', redirectTo: 'index.html', pathMatch: 'full' },
+];
